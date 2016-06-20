6ec44016ee36ed6fb324723bf385485a44b5bbaf
jeffryporter <jporter7@gmail.com>
2016-06-20 11:42:53 -0400

    Initial commit

---
+++ src/main/java/com/theironyard/Person.java
@@ -0,0 +1,19 @@
+package com.theironyard;
+
+/**
+ * Created by jeffryporter on 6/20/16.
+ */
+public class Person
+{
+    String name;
+    String city;
+    Integer age;
+
+    public Person(String name, String city, Integer age)
+    {
+        this.name = name;
+        this.city = city;
+        this.age = age;
+    }
+
+}
