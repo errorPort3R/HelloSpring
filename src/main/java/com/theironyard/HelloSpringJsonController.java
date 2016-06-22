a38ac23b2fcb39d28c3fbd9856e41c2ce9029a5d
jeffryporter <jporter7@gmail.com>
2016-06-22 11:04:06 -0400

    Add json

---
+++ src/main/java/com/theironyard/HelloSpringJsonController.java
@@ -0,0 +1,28 @@
+package com.theironyard;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jeffryporter on 6/22/16.
+ */
+
+@RestController
+public class HelloSpringJsonController
+{
+    @RequestMapping(path = "/person.json", method = RequestMethod.GET)
+    public Person getPerson()
+    {
+        Person p = new Person("Alice", "Charleston", 40);
+        return p;
+    }
+
+    @RequestMapping(path = "/person.json", method = RequestMethod.POST)
+    public void addPerson(@RequestBody Person person)
+    {
+        System.out.println("Recieved: " + person.name);
+    }
+
+}
--- src/main/java/com/theironyard/Person.java
+++ src/main/java/com/theironyard/Person.java
@@ -9,6 +9,10 @@ public class Person
     String city;
     Integer age;
 
+    public Person()
+    {
+    }
+
     public Person(String name, String city, Integer age)
     {
         this.name = name;
@@ -16,4 +20,33 @@ public class Person
         this.age = age;
     }
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getCity()
+    {
+        return city;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public Integer getAge()
+    {
+        return age;
+    }
+
+    public void setAge(Integer age)
+    {
+        this.age = age;
+    }
 }
